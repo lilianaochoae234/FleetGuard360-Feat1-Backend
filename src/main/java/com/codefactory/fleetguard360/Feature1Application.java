05327ab562b4518a88f4cf7fdf43a38d40b5c113
Manuel <juanrestrepo1221@gmail.com>
2025-05-10 23:15:42 -0500

    commit

---
+++ src/main/java/com/codefactory/fleetguard360/Feature1Application.java
@@ -0,0 +1,13 @@
+package com.codefactory.fleetguard360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feature1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Feature1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codefactory/fleetguard360/Feature1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefactory.fleetguard360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feature1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
